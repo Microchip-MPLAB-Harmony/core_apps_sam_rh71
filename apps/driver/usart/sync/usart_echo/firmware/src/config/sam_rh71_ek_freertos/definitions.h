940e43b5eccc940a55d18fb22fe2318a701147a1
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-05-11 18:11:31 +0530

    [DRV_USART] Regenerated demo for driver_usart_sync_usart_echo_sam_rh71_ek_freertos
    MH3-76989

--- apps/driver/usart/sync/usart_echo/firmware/src/config/sam_rh71_ek_freertos/definitions.h
+++ apps/driver/usart/sync/usart_echo/firmware/src/config/sam_rh71_ek_freertos/definitions.h
@@ -56,12 +56,12 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "peripheral/flexcom/usart/plib_flexcom1_usart.h"
+#include "bsp/bsp.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "system/dma/sys_dma.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "bsp/bsp.h"
 #include "app_usart_echo.h"
 
 
@@ -73,6 +73,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAMRH71F20C"
+#define DEVICE_ARCH			 "CORTEX-M7"
+#define DEVICE_FAMILY		 "SAMRH"
+#define DEVICE_SERIES		 "SAMRH71"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 100000000
--- apps/driver/usart/sync/usart_echo/firmware/src/config/sam_rh71_ek_freertos/initialization.c
+++ apps/driver/usart/sync/usart_echo/firmware/src/config/sam_rh71_ek_freertos/initialization.c
@@ -62,31 +62,35 @@
 // Section: Driver Initialization Data
 // *****************************************************************************
 // *****************************************************************************
+/* Following MISRA-C rules are deviated in the below code block */
+/* MISRA C-2012 Rule 11.1 */
+/* MISRA C-2012 Rule 11.3 */
+/* MISRA C-2012 Rule 11.8 */
 // <editor-fold defaultstate="collapsed" desc="DRV_USART Instance 0 Initialization Data">
 
 static DRV_USART_CLIENT_OBJ drvUSART0ClientObjPool[DRV_USART_CLIENTS_NUMBER_IDX0];
 
 
-const DRV_USART_PLIB_INTERFACE drvUsart0PlibAPI = {
+static const DRV_USART_PLIB_INTERFACE drvUsart0PlibAPI = {
     .readCallbackRegister = (DRV_USART_PLIB_READ_CALLBACK_REG)FLEXCOM1_USART_ReadCallbackRegister,
-    .read = (DRV_USART_PLIB_READ)FLEXCOM1_USART_Read,
+    .read_t = (DRV_USART_PLIB_READ)FLEXCOM1_USART_Read,
     .readIsBusy = (DRV_USART_PLIB_READ_IS_BUSY)FLEXCOM1_USART_ReadIsBusy,
     .readCountGet = (DRV_USART_PLIB_READ_COUNT_GET)FLEXCOM1_USART_ReadCountGet,
     .readAbort = (DRV_USART_PLIB_READ_ABORT)FLEXCOM1_USART_ReadAbort,
     .writeCallbackRegister = (DRV_USART_PLIB_WRITE_CALLBACK_REG)FLEXCOM1_USART_WriteCallbackRegister,
-    .write = (DRV_USART_PLIB_WRITE)FLEXCOM1_USART_Write,
+    .write_t = (DRV_USART_PLIB_WRITE)FLEXCOM1_USART_Write,
     .writeIsBusy = (DRV_USART_PLIB_WRITE_IS_BUSY)FLEXCOM1_USART_WriteIsBusy,
     .writeCountGet = (DRV_USART_PLIB_WRITE_COUNT_GET)FLEXCOM1_USART_WriteCountGet,
     .errorGet = (DRV_USART_PLIB_ERROR_GET)FLEXCOM1_USART_ErrorGet,
     .serialSetup = (DRV_USART_PLIB_SERIAL_SETUP)FLEXCOM1_USART_SerialSetup
 };
 
-const uint32_t drvUsart0remapDataWidth[] = { 0x0, 0x40, 0x80, 0xC0, 0x20000 };
-const uint32_t drvUsart0remapParity[] = { 0x800, 0x0, 0x200, 0x600, 0x400, 0xC00 };
-const uint32_t drvUsart0remapStopBits[] = { 0x0, 0x1000, 0x2000 };
-const uint32_t drvUsart0remapError[] = { 0x20, 0x80, 0x40 };
+static const uint32_t drvUsart0remapDataWidth[] = { 0x0, 0x40, 0x80, 0xC0, 0x20000 };
+static const uint32_t drvUsart0remapParity[] = { 0x800, 0x0, 0x200, 0x600, 0x400, 0xC00 };
+static const uint32_t drvUsart0remapStopBits[] = { 0x0, 0x1000, 0x2000 };
+static const uint32_t drvUsart0remapError[] = { 0x20, 0x80, 0x40 };
 
-const DRV_USART_INIT drvUsart0InitData =
+static const DRV_USART_INIT drvUsart0InitData =
 {
     .usartPlib = &drvUsart0PlibAPI,
 
@@ -109,6 +113,7 @@ const DRV_USART_INIT drvUsart0InitData =
 };
 
 // </editor-fold>
+
 
 
 // *****************************************************************************
@@ -140,7 +145,7 @@ SYSTEM_OBJECTS sysObj;
 // *****************************************************************************
 // *****************************************************************************
 
-
+/* MISRAC 2012 deviation block end */
 
 /*******************************************************************************
   Function:
@@ -154,6 +159,8 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
 
   
     CLOCK_Initialize();
@@ -162,8 +169,9 @@ void SYS_Initialize ( void* data )
 
 
 
-
     MATRIX_Initialize();
+
+	RSWDT_REGS->RSWDT_MR = RSWDT_MR_WDDIS_Msk;	// Disable RSWDT 
 
 	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
 
@@ -171,15 +179,25 @@ void SYS_Initialize ( void* data )
 
 	BSP_Initialize();
 
+
+    /* MISRAC 2012 deviation block start */
+    /* Following MISRA-C rules deviated in this block  */
+    /* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+    /* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
+
     sysObj.drvUsart0 = DRV_USART_Initialize(DRV_USART_INDEX_0, (SYS_MODULE_INIT *)&drvUsart0InitData);
 
 
 
 
+    /* MISRAC 2012 deviation block end */
     APP_USART_ECHO_Initialize();
 
 
     NVIC_Initialize();
+
+
+    /* MISRAC 2012 deviation block end */
 
 }
 
