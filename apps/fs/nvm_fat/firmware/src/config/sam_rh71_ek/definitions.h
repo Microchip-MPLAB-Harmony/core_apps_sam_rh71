a0ea037dec2ac39e024d28f9fea24bc9ba806a76
Syam Nair <Syam.Nair@microchip.com>
2025-09-23 09:20:02 +0000

    [APP] Regenerated demo for fs__nvm_fat__firmware
    MH3C-352

--- apps/fs/nvm_fat/firmware/src/config/sam_rh71_ek/definitions.h
+++ apps/fs/nvm_fat/firmware/src/config/sam_rh71_ek/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -62,11 +62,11 @@
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/mpu/plib_mpu.h"
 #include "driver/memory/drv_memory.h"
+#include "bsp/bsp.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "bsp/bsp.h"
 #include "app.h"
 
 
